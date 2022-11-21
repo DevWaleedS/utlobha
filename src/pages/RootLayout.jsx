6a1b9c2cbbaa3995b6931634d993377e0cbbb8da
DevWaleedS <eng.waleed.salah@outlook.com>
2022-11-21 13:35:18 +0200

    custom sidebar styles

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -15,7 +15,7 @@ const RootLayout = () => {
 							<div className='col-lg-2 col-md-3 col-sm-3 col-3'>
 								<SideBar />
 							</div>
-							<div className='col-lg-10 col-md-9 col-sm-9 col-9 pl'>
+							<div className='col-lg-10 col-md-9 col-sm-9 col-9 '>
 								<div className='main-content'>
 									<Outlet />
 								</div>
