ac2bc58356d7c8c667b551470fd8f3b78baea73a
DevWaleedS <eng.waleed.salah@outlook.com>
2022-11-24 00:14:55 +0200

    last commit datepicker

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -15,7 +15,7 @@ const RootLayout = () => {
 							<div className='col-lg-2 col-md-3 col-sm-3 col-3'>
 								<SideBar />
 							</div>
-							<div className='col-lg-10 col-md-9 col-sm-9 col-9 '>
+							<div className='col-lg-10 col-md-9 col-sm-9 col-9 pages-content'>
 								<div className='main-content'>
 									<Outlet />
 								</div>
