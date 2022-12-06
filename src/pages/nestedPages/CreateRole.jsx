6a2b809b0be19e082d9a951612b64dc883a9c53b
DevWaleedS <eng.waleed.salah@outlook.com>
2022-12-07 01:25:05 +0200

    commit

--- src/pages/nestedPages/CreateRole.jsx
+++ src/pages/nestedPages/CreateRole.jsx
@@ -1,50 +1,100 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-
-import { Link } from 'react-router-dom';
-import arrowBack from '../../data/Icons/icon-30-arrwos back.svg';
+import { Link, useNavigate } from 'react-router-dom';
 
 // MUI
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
+import Switch from '@mui/material/Switch';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Toolbar from '@mui/material/Toolbar';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 // ICONS
-import { ReactComponent as StatusIcon } from '../../data/Icons/status.svg';
-import { ReactComponent as DateIcon } from '../../data/Icons/icon-date.svg';
-import { ReactComponent as TypeSuport } from '../../data/Icons/type support.svg';
-import { ReactComponent as Client } from '../../data/Icons/icon-24-user.svg';
-import { ReactComponent as Customer } from '../../data/Icons/icon-support.svg';
-import { ReactComponent as Phone } from '../../data/Icons/icon-24- call.svg';
-
-import { ReactComponent as BoldIcon } from '../../data/Icons/icon-24-Bold.svg';
-import { ReactComponent as FormatTextCenter } from '../../data/Icons/icon-24-format text center.svg';
-import { ReactComponent as FormatTextLeft } from '../../data/Icons/icon-24-format text lift.svg';
-import { ReactComponent as FormatTextRight } from '../../data/Icons/icon-24-format text right.svg';
-import { ReactComponent as FormatTextPoint } from '../../data/Icons/icon-24-format text point.svg';
-import { ReactComponent as FormatTextPointSqure } from '../../data/Icons/icon-24-format text-point.svg';
-import { ReactComponent as Attchment } from '../../data/Icons/icon-5.svg';
+import { ReactComponent as StarIcon } from '../../data/Icons/icon-20-star.svg';
+import arrowBack from '../../data/Icons/icon-30-arrwos back.svg';
 
 // Modal Style
 const style = {
 	position: 'absolute',
 	top: '97px',
-	left: '-1%',
+	left: '0',
 	transform: 'translate(0%, 0%)',
-	width: '80%',
+	width: '100%',
 	height: '100%',
 	overflow: 'auto',
 	bgcolor: '#f8f9fa',
-
 	paddingBottom: '200px',
 };
 
+const userDataTable = [
+	{
+		id: 1,
+		title: 'عدد المستخدمين',
+	},
+	{
+		id: 2,
+		title: 'رقم الجوال',
+	},
+	{
+		id: 3,
+		title: ' البريد الالكتروني',
+	},
+];
+
+const mainData = [
+	{
+		id: 1,
+		title: ' ملخص الشهر',
+	},
+	{
+		id: 2,
+		title: ' ملخص طلبات الاشتراكات',
+	},
+	{
+		id: 3,
+		title: '  التنبيهات',
+	},
+];
+
+const roleData = [
+	{
+		id: 1,
+		title: '  مستخدمي المتجر',
+	},
+];
+
+function EnhancedTableToolbar({ label }) {
+	return (
+		<Toolbar
+			className=' justify-content-start'
+			sx={{
+				pl: { sm: 2 },
+				pr: { xs: 1, sm: 1 },
+
+				bgcolor: '#fff',
+			}}
+		>
+			<FormControlLabel control={<Switch defaultChecked />} label={label} />
+		</Toolbar>
+	);
+}
+
 const CreateRole = () => {
-	const dispatch = useDispatch(false);
+	const navigate = useNavigate();
+
 	return (
 		<div className='' open={true}>
 			<Modal open={true} aria-labelledby='modal-modal-title' aria-describedby='modal-modal-description'>
 				<Box sx={style} className='nested-pages-modal'>
-					<section className='SupportDetails-page '>
+					<section className='SupportDetails-page create-role'>
 						<div className='head-category mb-5 '>
 							<div className='row'>
 								<div className='page-title mb-3'>
@@ -67,6 +117,213 @@ const CreateRole = () => {
 										</li>
 									</ol>
 								</nav>
+							</div>
+						</div>
+
+						<div className='create-role-wrapper'>
+							<div className='row mb-5'>
+								<div className='search-wrapper'>
+									<div className='col-6'>
+										<label className='d-block mb-2' htmlFor='role-search'>
+											<StarIcon className='star-icon' />
+											<span className='me-2'>اسم الدور الوظيفي</span>
+										</label>
+										<input style={{ backgroundColor: '#fff' }} type='text' name='role-search' id='role-search' placeholder='ادخل اسم الدور الوظيفي بالحروف فقط' />
+									</div>
+								</div>
+							</div>
+
+							<div className='row mb-5'>
+								<div className='create-role-title mb-4'>
+									<StarIcon className='star-icon' />
+									<span className='me-2'> حدد الصلاحيات</span>
+								</div>
+
+								<div className='create-role-tables'>
+									<div className='role-wrapper mb-5'>
+										<div className='row'>
+											<EnhancedTableToolbar label='عرض بيانات المستخدمين' />
+											<TableContainer component={Paper} sx={{ p: 0 }}>
+												<Table>
+													<TableHead
+														sx={{
+															bgcolor: '#eff9ff',
+														}}
+													>
+														<TableRow>
+															<TableCell
+																sx={{
+																	width: '60%',
+																}}
+																align='right'
+															>
+																اسم الصلاحية
+															</TableCell>
+															<TableCell align='center'>عرض</TableCell>
+															<TableCell align='center'>تعديل</TableCell>
+															<TableCell align='center'>إضافة</TableCell>
+															<TableCell align='center'>حذف</TableCell>
+														</TableRow>
+													</TableHead>
+													<TableBody>
+														{userDataTable.map((row) => (
+															<TableRow key={row.id}>
+																<TableCell
+																	sx={{
+																		border: 'none',
+																		width: '60%',
+																	}}
+																	align='right'
+																>
+																	{row.title}
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+															</TableRow>
+														))}
+													</TableBody>
+												</Table>
+											</TableContainer>
+										</div>
+									</div>
+
+									<div className='role-wrapper mb-5'>
+										<div className='row'>
+											<EnhancedTableToolbar label='الرئيسية' />
+											<TableContainer component={Paper} sx={{ p: 0 }}>
+												<Table>
+													<TableHead
+														sx={{
+															bgcolor: '#eff9ff',
+														}}
+													>
+														<TableRow>
+															<TableCell
+																sx={{
+																	width: '60%',
+																}}
+																align='right'
+															>
+																اسم الصلاحية
+															</TableCell>
+															<TableCell align='center'>عرض</TableCell>
+															<TableCell align='center'>تعديل</TableCell>
+															<TableCell align='center'>إضافة</TableCell>
+															<TableCell align='center'>حذف</TableCell>
+														</TableRow>
+													</TableHead>
+													<TableBody>
+														{mainData.map((row) => (
+															<TableRow key={row.id}>
+																<TableCell
+																	sx={{
+																		border: 'none',
+																		width: '60%',
+																	}}
+																	align='right'
+																>
+																	{row.title}
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+															</TableRow>
+														))}
+													</TableBody>
+												</Table>
+											</TableContainer>
+										</div>
+									</div>
+									<div className='role-wrapper mb-5'>
+										<div className='row'>
+											<EnhancedTableToolbar label='المستخدمين' />
+											<TableContainer component={Paper} sx={{ p: 0 }}>
+												<Table>
+													<TableHead
+														sx={{
+															bgcolor: '#eff9ff',
+														}}
+													>
+														<TableRow>
+															<TableCell
+																sx={{
+																	width: '60%',
+																}}
+																align='right'
+															>
+																اسم الصلاحية
+															</TableCell>
+															<TableCell align='center'>عرض</TableCell>
+															<TableCell align='center'>تعديل</TableCell>
+															<TableCell align='center'>إضافة</TableCell>
+															<TableCell align='center'>حذف</TableCell>
+														</TableRow>
+													</TableHead>
+													<TableBody>
+														{roleData.map((row) => (
+															<TableRow key={row.id}>
+																<TableCell
+																	sx={{
+																		border: 'none',
+																		width: '60%',
+																	}}
+																	align='right'
+																>
+																	{row.title}
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+																<TableCell sx={{ border: 'none' }} align='center'>
+																	<Checkbox />
+																</TableCell>
+															</TableRow>
+														))}
+													</TableBody>
+												</Table>
+											</TableContainer>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className='row d-flex justify-content-center'>
+							<div className='col-2 '>
+								<div className='btn-wrapper w-100'>
+									<button
+										onClick={() => {
+											navigate('/Management/JobTitles');
+										}}
+									>
+										حفظ واعتماد
+									</button>
+								</div>
 							</div>
 						</div>
 					</section>
